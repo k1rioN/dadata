dd8c67b948ea315d1afb17375522a406caadf3f6
k1rioN <58228299+k1rioN@users.noreply.github.com>
2021-10-08 01:07:39 +0300

    third

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1>ВСЕ РАБОТАЕТ!</h1>
+    <div class="div">
+        <input class="inputs" style="height:70px; width:500px">
+        <button v-on:click="GetData" style="height:73px; width:100px">Поиск</button>
+    </div>
 </template>
 
 <script>
+var url = "https://cleaner.dadata.ru/api/v1/clean/address";
+var token = "37ea5e15a9c61b401af9b23bee9563ef7654f126";
+var secret = "26ed63e476256db8bf5e7ffa390107ff681f7860";
+var query = "мск сухонска 11/-89";
+
+var options = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Token " + token,
+        "X-Secret": secret
+    },
+    body: JSON.stringify([query])
+}
+
 export default {
-  
+    async GetData({$axios}) {
+        let {data} = await $axios.get('https://cleaner.dadata.ru/api/v1/clean/address', options)
+        return {data}
+        console.log(data)
+    }
 }
 </script>
+
+
+
