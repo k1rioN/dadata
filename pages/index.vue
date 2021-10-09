20232e1b32ecf9779200ea9d2a821081dadeb17e
k1rioN <58228299+k1rioN@users.noreply.github.com>
2021-10-10 00:53:42 +0300

    index change input

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
-    <div class="div">
-        <input class="inputs" style="height:70px; width:500px">
-        <button v-on:click="GetData" style="height:73px; width:100px">Поиск</button>
-    </div>
+  <div>
+    <input class="inputs" style="height:70px; width:500px">
+  </div>
 </template>
-
 <script>
 var url = "https://cleaner.dadata.ru/api/v1/clean/address";
 var token = "37ea5e15a9c61b401af9b23bee9563ef7654f126";
 var secret = "26ed63e476256db8bf5e7ffa390107ff681f7860";
 var query = "мск сухонска 11/-89";
-
 var options = {
     method: "POST",
     mode: "cors",
@@ -21,13 +18,11 @@ var options = {
     },
     body: JSON.stringify([query])
 }
-
 export default {
-    async GetData({$axios}) {
-        let {data} = await $axios.get('https://cleaner.dadata.ru/api/v1/clean/address', options)
-        return {data}
-        console.log(data)
-    }
+  async asyncData({ $axios }) {
+    const mountains = await $axios.$get('https://cleaner.dadata.ru/api/v1/clean/address', options, query)
+    return { mountains }
+  }
 }
 </script>
 
